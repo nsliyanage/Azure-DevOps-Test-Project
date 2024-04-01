52174d020339f0dab410edb9233c53803426aa55
Nethra Sewwandi <97428958+nsliyanage@users.noreply.github.com>
2024-04-01 13:52:04 +0530

    Add files via upload

---
+++ VS Test Nethra/VS Test Nethra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VS_Test_Nethra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
